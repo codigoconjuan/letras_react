d5e393f7efbeeb807a354769316cf9acdfc711e8
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-12 19:15:16 -0600

    finalizando el proyecto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cancion from './components/Cancion';
+import Info from './components/Info';
 import axios from 'axios';
 
 function App() {
@@ -24,12 +25,12 @@ function App() {
       ]);
 
       guardarLetra(letra.data.lyrics);
-      guardarInfo(informacion.data.artist[0]);
+      guardarInfo(informacion.data.artists[0]);
 
       // guardarLetra(resultado.data.lyrics);
     }
     consultarApiLetra();
-  }, [busquedaletra]);
+  }, [busquedaletra, info]);
 
   return (
       <Fragment>
@@ -40,7 +41,9 @@ function App() {
           <div className="container mt-5">
             <div className="row">
               <div className="col-md-6">
-
+                <Info 
+                  info={info}
+                />
               </div>
               <div className="col-md-6">
                   <Cancion 
---
+++ src/components/Info.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Info = ({info}) => {
+
+    if(Object.keys(info).length === 0 ) return null;
+
+    const { strArtistThumb, strGenre, strBiographyES } = info;
+    
+    return ( 
+        <div className="card border-light">
+            <div className="card-header bg-primary text-light font-weight-bold">
+                Información Artista
+            </div>
+            <div className="card-body">
+                <img src={strArtistThumb} alt="Logo Artista" />
+                <p className="card-text">Género: {strGenre}</p>
+                <h2 className="card-text">Biografía:</h2>
+                <p className="card-text">{strBiographyES}</p>
+                <p className="card-text">
+                    <a href={`https://${info.strFacebook}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                    <a href={`https://${info.strTwitter}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href={`${info.strLastFMChart}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-lastfm"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Info;
